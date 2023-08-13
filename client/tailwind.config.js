c75df7535ab98056223af6303c7100512579ad48
Troy Dorman <troydorman@gmail.com>
2023-08-13 12:00:35 -0600

    dropdown box 1

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+
+
+
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,4 +18,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
