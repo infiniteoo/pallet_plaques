b8c65d10db7dbaafd86615af365bc3decf2ea749
Troy Dorman <troydorman@gmail.com>
2023-08-15 11:46:18 -0600

    updating the pallet count in the definedStops array

--- client/app/components/NumberOfPalletsDropDown.jsx
+++ client/app/components/NumberOfPalletsDropDown.jsx
@@ -1,48 +1,64 @@
-"use client"
+"use client";
 
-import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import palletCount from './palletCount.js'
+import { Fragment, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import palletCount from "./palletCount.js";
 
-
-
-  
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
+  return classes.filter(Boolean).join(" ");
+}
 
+const NumberOfPalletsDropDown = ({
+  stop,
+  definedStops,
+  setDefinedStops,
+  numberOfStops,
+  setNumberOfStops,
+}) => {
+  console.log("stop", stop)
+  const stopIndex = stop.stopNumber - 1; // Assuming stops are 1-indexed
 
-const NumberOfPalletsDropDown = () => {
-    const [selected, setSelected] = useState(palletCount[3])
+  const [selected, setSelected] = useState(palletCount[3]);
 
+  const handlePalletCountChange = (selectedPalletCount) => {
+    console.log('selectedPalletCount', selectedPalletCount)
+    setSelected(selectedPalletCount);
 
-
-
+    // Update the corresponding definedStops object with the selected pallet count
+    setDefinedStops((prevDefinedStops) => {
+      const updatedStops = [...prevDefinedStops];
+      updatedStops[stopIndex] = {
+        ...updatedStops[stopIndex],
+        numberOfPallets: selectedPalletCount.id, // Assuming the selectedPalletCount object has a 'value' property
+      };
+      return updatedStops;
+    });
+  };
 
   return (
-     <Listbox value={selected} onChange={
-          (selected) => {
-            setSelected(selected)
-            console.log('selected', selected)
-          }
-        }>
-
-      
-      
-     
+    <Listbox
+      value={selected.id}
+      onChange={handlePalletCountChange}
+    >
       {({ open }) => (
         <>
-        
           <div className="relative mt-2">
-          <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">Number of Pallets</label>
+            <label
+              htmlFor="price"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Number of Pallets
+            </label>
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
-                
                 <span className="ml-3 block truncate">{selected.id}</span>
               </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                <ChevronUpDownIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
               </span>
             </Listbox.Button>
 
@@ -59,8 +75,8 @@ const NumberOfPalletsDropDown = () => {
                     key={person.id}
                     className={({ active }) =>
                       classNames(
-                        active ? 'bg-indigo-600 text-white' : 'text-gray-900',
-                        'relative cursor-default select-none py-2 pl-3 pr-9'
+                        active ? "bg-indigo-600 text-white" : "text-gray-900",
+                        "relative cursor-default select-none py-2 pl-3 pr-9"
                       )
                     }
                     value={person}
@@ -68,9 +84,11 @@ const NumberOfPalletsDropDown = () => {
                     {({ selected, active }) => (
                       <>
                         <div className="flex items-center">
-                         
                           <span
-                            className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
+                            className={classNames(
+                              selected ? "font-semibold" : "font-normal",
+                              "ml-3 block truncate"
+                            )}
                           >
                             {person.id}
                           </span>
@@ -79,8 +97,8 @@ const NumberOfPalletsDropDown = () => {
                         {selected ? (
                           <span
                             className={classNames(
-                              active ? 'text-white' : 'text-indigo-600',
-                              'absolute inset-y-0 right-0 flex items-center pr-4'
+                              active ? "text-white" : "text-indigo-600",
+                              "absolute inset-y-0 right-0 flex items-center pr-4"
                             )}
                           >
                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
@@ -96,7 +114,7 @@ const NumberOfPalletsDropDown = () => {
         </>
       )}
     </Listbox>
-  )
-}
+  );
+};
 
-export default NumberOfPalletsDropDown
+export default NumberOfPalletsDropDown;
