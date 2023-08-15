f8d0fe74ef2dd6794c20ca8f70a709bee09d05c8
Troy Dorman <troydorman@gmail.com>
2023-08-15 13:34:26 -0600

    added logo

--- client/app/page.js
+++ client/app/page.js
@@ -3,6 +3,7 @@
 import CompleteStopGroup from "./components/CompleteStopGroup.jsx";
 import SubmitButton from "./components/SubmitButton.jsx";
 import { useState, useEffect } from "react";
+import GXOLogo from "./components/assets/GXO_logo.png"; // Import the logo image
 
 export default function Home() {
   const [numberOfStops, setNumberOfStops] = useState(1);
@@ -44,24 +45,27 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
-        {/* Map each stop to a new row */}
-        {definedStops.map((stop) => (
-          <div
-            key={stop.stopNumber}
-            className="flex flex-row items-center justify-between w-full"
-          >
-            <CompleteStopGroup
-              stop={stop}
-              numberOfStops={numberOfStops}
-              setNumberOfStops={setNumberOfStops}
-              definedStops={definedStops}
-              setDefinedStops={setDefinedStops}
-            />
-          </div>
-        ))}
-        <SubmitButton definedStops={definedStops} />
-      </div>
-    </main>
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex flex-col">
+      {/* Display the logo image */}
+      <img src={"/GXO_logo.png"} alt="GXO Logo" className="w-32 h-auto mb-4" />
+
+      {/* Map each stop to a new row */}
+      {definedStops.map((stop) => (
+        <div
+          key={stop.stopNumber}
+          className="flex flex-row items-center justify-between w-full"
+        >
+          <CompleteStopGroup
+            stop={stop}
+            numberOfStops={numberOfStops}
+            setNumberOfStops={setNumberOfStops}
+            definedStops={definedStops}
+            setDefinedStops={setDefinedStops}
+          />
+        </div>
+      ))}
+      <SubmitButton definedStops={definedStops} />
+    </div>
+  </main>
   );
 }
