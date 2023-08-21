a53bc6a7db126eff4ae6d9571d708684d0896b3c
Troy Dorman <troydorman@gmail.com>
2023-08-21 14:55:50 -0600

    updated fake store names with real store names

--- client/app/components/storeNames.js
+++ client/app/components/storeNames.js
@@ -1,61 +1,82 @@
  export  const storeNames = [
     {
       id: 1,
-      name: 'Costco 0113 SLC II',
+      name: '500782 - COSTCO SALT LAKE 113',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 2,
-      name: 'Costco 0243 Orem',
+      name: '500792 - COSTCO OREM 0484',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 3,
-      name: 'Costco 0213 Lehi',
+      name: '500791 - COSTCO SANDY UTAH 0487',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 4,
-      name: 'Costco 2144 Saratoga Springs',
+      name: '500797 - COSTCO WEST VALLEY 0622',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 5,
-      name: 'Costco 5522 S. Jordan',
+      name: '500793 - COSTCO LEHI 0733',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 6,
-      name: 'Costco 2243 St. George',
+      name: '500795 - COSTCO BOUNTIFUL 0735',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 7,
-      name: 'Costco 0533 Murray',
+      name: '500794 - COSTCO MURRAY 0764',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 8,
-      name: 'Costco 5232 Ogden',
+      name: '500796 - COSTCO OGDEN 0770',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 9,
-      name: 'Costco 9913 Spanish Fork',
+      name: '501022 - COSTCO SOUTH JORDAN 1019',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
     {
       id: 10,
-      name: 'Costco 3302 Burley',
+      name: '500992 - COSTCO SPANISH FORK 1118',
+      avatar:
+        'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
+    },
+    ,
+    {
+      id: 11,
+      name: '500992 - COSTCO SARATOGA SPRINGS 1383',
+      avatar:
+        'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
+    },
+    ,
+    {
+      id: 12,
+      name: '501098 - COSTCO RIVERTON 1441',
+      avatar:
+        'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
+    },
+    ,
+    {
+      id: 13,
+      name: '222012 - COSTCO LOGAN 1633',
       avatar:
         'https://mobilecontent.costco.com/live/resource/img/static-us-landing-pages/21w1353-app-icon.png',
     },
